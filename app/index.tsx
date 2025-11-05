21d8f97d0e7a18953e7549583015a297c0af73e2
Borys Borejszo <borys@MacBook-Air-Borys.local>
2025-11-05 12:58:58 +0100

    test

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-500 text-2xl font-bold">
+        NativeWind działa 🎉
+      </Text>
+    </View>
+  );
+}

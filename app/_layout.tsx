1f1d76200c7f20e2893145c82c78235612ba2268
Borys Borejszo <borys@MacBook-Air-Borys.local>
2025-11-05 14:48:52 +0100

    zaczeta strona glowna i dolny pasek nawigacji

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { View } from "react-native";
 
 export default function Layout() {
-  return <Stack />;
+  return (
+    <>
+      <Stack />
+      <View className="flex-1 bg-black absolute bottom-0 w-full h-24 border-t-2 border-gray-700"></View>
+    </>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,58 @@
-import { View, Text } from "react-native";
+import { SafeAreaView, ScrollView, Text, View } from "react-native";
 
 export default function App() {
+  const today = new Date();
+  const formattedDate = today.toLocaleDateString("pl-PL", {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-blue-500 text-2xl font-bold">
-        NativeWind działa 🎉
-      </Text>
-    </View>
+    <SafeAreaView className="flex-1 bg-black">
+      <ScrollView
+        contentContainerStyle={{ paddingBottom: 48 }}
+        showsVerticalScrollIndicator={false}
+      >
+        <View className="flex-1">
+          {/* Header (greeting + date) */}
+          <View className="pt-4 px-4">
+            <Text className="text-white text-3xl font-bold">Witaj, User!</Text>
+            <Text className="text-white text-2xl mt-1">{formattedDate}</Text>
+          </View>
+
+          {/* Two small cards aligned left and right using flex-row */}
+          <View className="mt-4 px-4 flex-row justify-between items-start">
+            <View className="w-48 h-36 bg-black border-gray-700 border rounded-xl shadow shadow-blue-200" />
+            <View className="w-48 h-36 bg-black border-gray-700 border rounded-xl shadow shadow-blue-200" />
+          </View>
+
+          {/* Title + big schedule card */}
+          <View className="px-4 mt-4">
+            <Text className="text-white text-2xl">Dzisiejszy plan lekcji:</Text>
+            <View className="mt-3 border-gray-700 border rounded-xl w-full h-96 bg-black shadow shadow-blue-200" />
+          </View>
+
+          <View>
+            <Text className="text-white text-2xl pt-4 px-4">Nowe aktualizacje:</Text>
+          </View>
+
+          <View className="px-4 mt-4">
+            <View className="mt-3 border-gray-700 border rounded-xl w-full h-96 bg-black shadow shadow-blue-200" />
+          </View>
+
+          
+
+
+
+
+
+
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
+

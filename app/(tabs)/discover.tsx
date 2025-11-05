52e07e5d431e99d31e86aef3757a37d59402e0fa
Borys Borejszo <borys@MacBook-Air-Borys.local>
2025-11-05 09:39:33 +0100

    d

---
+++ app/(tabs)/discover.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const discover = () => {
+    return (
+        <View>
+            <Text>Discover</Text>
+        </View>
+    );
+};
+
+export default discover;
